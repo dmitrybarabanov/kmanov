f80dc258a4c6709bcdbbd2505dfb6207221a2045
dmitry <d.a.barabanov@gmail.com>
2017-06-21 15:45:10 +0300

    шапка

--- files/js/init.js
+++ files/js/init.js
@@ -3,5 +3,52 @@ var mobileWidth = 766;
 jQuery(document).ready(function( $ ) {
 
 	is_mobile = ($(window).width()<=mobileWidth);
+	
+	
+	
+	
+	$('nav').attr("data-width", $('nav').outerWidth());
+	
+	_init_top_menu();
+	
+	_default_init();
+	
 
 });
+
+
+function _init_top_menu()
+{
+	//top menu
+	$(window).on("scroll", function () {
+		_default_init();
+	});
+}
+
+function _default_init()
+{
+	var scrolled = $(this).scrollTop();
+	if( scrolled > 100 ) {
+		$('.for_mobile_wrapper').addClass('fix');
+		
+		
+	}   
+	if( scrolled <= 100 ) {     
+		$('.for_mobile_wrapper').removeClass('fix'); 
+	}
+	
+	/*
+	var scrolled = $(this).scrollTop();
+	if( scrolled > 100 ) {
+		$('nav').addClass('fix');
+		$('.logo').addClass('fix');
+		
+		$('nav').css("right",$('nav').data("width")+"px");
+		
+		
+	}   
+	if( scrolled <= 100 ) {     
+		$('nav').removeClass('fix');
+		$('.logo').removeClass('fix'); 
+	}*/
+}
